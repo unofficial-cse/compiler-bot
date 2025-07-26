a50d3ac9e3bf3099e6bcacddf737229c28737851
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2025-07-26 22:26:12 +0800

    feat: nix flake new

--- flake.nix
+++ flake.nix
@@ -1,7 +1,35 @@
 {
-    description = "Flake for Unofficial.CSE compiler-bot";
+  description = "Flake for Unofficial.CSE compiler-bot";
 
-    inputs = {
-        nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    };
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        toolchain = (pkgs.rustChannelOf {
+          rustToolchain = ./rust-toolchin;
+          sha256 = "";
+        }).rust;
+
+        naersk-lib = pkgs.callPackage naersk {
+          cargo = toolchain;
+          clippy = toolchain;
+          rustc = toolchain;
+          rustfmt = toolchain;
+        };
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs; mkShell {
+          buildInputs = [ toolchain ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+      }
+    );
 }
